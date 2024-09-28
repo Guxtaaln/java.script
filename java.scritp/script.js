24c74cd845867402cf80f2bf8d70861d13755b64
Guxtaaln <gustavoalmeidanascimento22@gmail.com>
2024-09-28 11:58:09 -0300

    Add files via upload

---
+++ java.scritp/script.js
@@ -0,0 +1,11 @@
+function solicitardados() {
+    // solicita ola jovem
+    let recepção = prompt("Olá, seja bem vindo");
+    // solicita uma pergunta
+    let pergunta = prompt(" Como esta sendo seu dia?");
+    // solicita uma respotar 
+    alert(" seja bem vindo" + recepção + "seja bem vindo" + pergunta + "sendo seu dia. "); -
+        // meu console.log
+        console.log("seja bem vindo", recepção);
+    console.log("sendo seu dia ", pergunta);
+}  
